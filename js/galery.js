f5c0e16189b4185932a01196101dabf69d8a8f8d
jota0222 <jdherandez@gmail.com>
2017-10-25 14:35:54 -0500

    Se agregó código para adquisición de productos en js

---
+++ js/galery.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    let root = 'https://jsonplaceholder.typicode.com';
+    let products;
+
+    /**
+     * Función para adquirir todos los elementos de un tipo de producto definido.
+     * 
+     * @param {string} productType - Tipo de productos que se pedirán.
+     * @returns Una promesa ajax para suscibirse
+     */
+    function getAllProductsByType(productType) {
+        return $.ajax({
+            url: root + '/' + productType,
+            method: 'GET'
+        });
+    }
+
+    getAllProductsByType("todos")
+        .then(function (data) {
+            products = data;
+            console.log(data);
+        });
+
+});
