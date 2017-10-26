626106adf007b344aecb20d3d39dae0919597814
alejobg <alejo.arboh6@gmail.com>
2017-10-25 22:22:26 -0500

    detalle producto version 15

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-var producto = {id="-1", nombre:"No encontrado", precio:0, descripcion:"", calificacion=0};
+var producto = {id:"-1", nombre:"No encontrado", precio:0, descripcion:"No tiene", calificacion:0};
 
 
 $( document ).ready(function() {
 
 	var boton
 
-	$("#li_perfil").click(function(){
-	     
+	$("#btn_añadir_carro").click(function(){
+	     localStorage.setItem("productoCarro", JSON.stringify(producto));
 
 	});
 
 
-	$("#li_perfil").click(function(){
-	      window.location.href='perfil.html';
+	$("#btn_anadir_deseos").click(function(){
+	       localStorage.setItem("productoDeseos", JSON.stringify(producto));
 
 	});
 
