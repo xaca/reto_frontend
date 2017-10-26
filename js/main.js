88f5a7c4664d35000f0efccfb3adf3d94fd69ede
alejobg <alejo.arboh6@gmail.com>
2017-10-25 22:07:54 -0500

    detalle producto version 14

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var producto = {id="-1", nombre:"No encontrado", precio:0, descripcion:"", calificacion=0};
+
+
+$( document ).ready(function() {
+
+	var boton
+
+	$("#li_perfil").click(function(){
+	     
+
+	});
+
+
+	$("#li_perfil").click(function(){
+	      window.location.href='perfil.html';
+
+	});
+
+
+});
